d35e25d12cf5ae40ef7bfb4a0029c0c6cd97754e
Mateusz Podeszwa <podinatubie@gmail.com>
2024-01-31 06:53:29 +0000

    Clean-up the code
    Just a little clean-up, moved template to top, and scripts to middle. Also removes css code.

--- resources/js/PAGES/login-page.vue
+++ resources/js/PAGES/login-page.vue
@@ -1,3 +1,7 @@
+<template>
+
+</template>
+
 <script>
 export default {
     name: 'LoginPage',
@@ -7,41 +11,15 @@ export default {
         };
     },
     methods: {
-        goToLoginPage() {
-            // Use Vue Router to navigate to the 'LoginPage'
-            this.$router.push({ name: 'WelcomePage' })}
+
     }
 };
 </script>
-
-<template>
-    <router-link :to="{ name: 'WelcomePage' }">Go to Main</router-link>
-    <button class="custom-button" id="loginButton" @click="goToLoginPage">Go to Login Page</button>
-
-</template>
 
 <style scoped lang="sass">
 
 </style>
 
 <style scoped lang="css">
-.custom-button {
-    display: inline-block;
-    padding: 10px 20px;
-    font-size: 16px;
-    font-weight: bold;
-    text-align: center;
-    text-decoration: none;
-    cursor: pointer;
-    background-color: #3498db; /* Blue background color */
-    color: #fff; /* White text color */
-    border: none; /* Remove border */
-    border-radius: 5px; /* Rounded corners */
-    transition: background-color 0.3s; /* Smooth transition on hover */
 
-    /* Hover effect */
-    &:hover {
-        background-color: #2980b9; /* Darker blue on hover */
-    }
-}
 </style>
