69f880f8289d4c47cc90c516c18535060bb78f74
Mateusz Podeszwa <podinatubie@gmail.com>
2024-01-31 09:57:18 +0000

    Develop status login-page.vue 20%

--- resources/js/PAGES/login-page.vue
+++ resources/js/PAGES/login-page.vue
@@ -1,13 +1,20 @@
 <template>
 
-    <body class="w-full h-full">
-        <div class="flex items-center justify-center h-screen">
-            <div class="loginBox-BACKGROUND p-8 w-[300px] h-[300px]">
-                <!-- Logo -->
-                <mmw-logo-wide />
-                <input type="text" class="w-full" placeholder="Username">
-                <input type="password" class="w-full" placeholder="Password">
+    <body class="w-full h-screen">
+
+        <div class="background-SECTION h-screen">
+            <div class="w-[50%]"></div>
+            <div class="w-[50%]"></div>
+        </div>
+
+        <div class="loginBox-BACKGROUND loginBox-GRID_LAYOUT">
+            <div class="firsRow"><mmw-logo-wide /></div>
+
+            <div class="secondRow">
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
             </div>
+
         </div>
 
     </body>
@@ -34,19 +41,50 @@ import mmwLogoWide from '../COMPONENTS/mmw-logo-wide.vue';
 </script>
 
 <style scoped lang="sass">
-// Importing local files
-@use '../../sass/abstracts' as *
-@use "../../sass/base" as base
+    // Importing local files
+    @use '../../sass/abstracts' as *
+    @use "../../sass/base" as base
 
-// Define local variables
-$MainBackgroundColor: getColor(background, primary)
-$MainBackgroundComponentColor: getColor(backgroundComponents, BasicWindow)
+    // Define local variables
+    $MainBackgroundColor: getColor(background, primary)
+    $SecondaryBackgroundColor: getColor(background, secondary)
+    $MainBackgroundComponentColor: getColor(backgroundComponents, BasicWindow)
 
-body
-    background-color: $MainBackgroundColor
+    body
+        position: relative
+
+    .background-SECTION
+        background-color: #1a202c
+        z-index: 0
+        div:first-child
+            background-color: $SecondaryBackgroundColor
+            float: right
+        div:nth-child(2)
+            background-color: $MainBackgroundColor
+            float: left
+        div
+            height: 100vmin
 
     .loginBox-BACKGROUND
         background-color: $MainBackgroundComponentColor
+        position: absolute
+        top: 50%
+        left: 50%
+        transform: translate(-50%, -50%)
+        z-index: 1
+
+    .loginBox-GRID_LAYOUT
+        display: grid
+        grid-template-rows: 20% 80%
+        div
+            width: auto
+            height: auto
+            z-index: 2
+        .firsRow
+            grid-area: top
+        .secondRow
+            grid-area: bottom
+
 </style>
 
 <style scoped lang="css">
