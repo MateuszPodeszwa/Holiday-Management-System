998bba171aa4ba8c6caac2aa75a3a37fa2e8285f
Mateusz Podeszwa <podinatubie@gmail.com>
2024-02-02 14:02:37 +0000

    Add a login page

--- resources/js/PAGES/login-page.vue
+++ resources/js/PAGES/login-page.vue
@@ -1,10 +1,25 @@
 <template>
 
+    <body class="w-full h-full">
+        <div class="flex items-center justify-center h-screen">
+            <div class="loginBox-BACKGROUND p-8 w-[300px] h-[300px]">
+                <!-- Logo -->
+                <mmw-logo-wide />
+                <input type="text" class="w-full" placeholder="Username">
+                <input type="password" class="w-full" placeholder="Password">
+            </div>
+        </div>
+
+    </body>
+
 </template>
 
 <script>
+import MmwLogoWide from "../COMPONENTS/mmw-logo-wide.vue";
+
 export default {
     name: 'LoginPage',
+    components: {MmwLogoWide},
     data() {
         return {
 
@@ -14,10 +29,24 @@ export default {
 
     }
 };
+
+import mmwLogoWide from '../COMPONENTS/mmw-logo-wide.vue';
 </script>
 
 <style scoped lang="sass">
+// Importing local files
+@use '../../sass/abstracts' as *
+@use "../../sass/base" as base
 
+// Define local variables
+$MainBackgroundColor: getColor(background, primary)
+$MainBackgroundComponentColor: getColor(backgroundComponents, BasicWindow)
+
+body
+    background-color: $MainBackgroundColor
+
+    .loginBox-BACKGROUND
+        background-color: $MainBackgroundComponentColor
 </style>
 
 <style scoped lang="css">
