af6e6f760cf5e26737b5b91d1da3169389ea48bf
Mateusz Podeszwa <podinatubie@gmail.com>
2024-02-14 08:58:39 +0000

    Add animation to acc. nav.

--- resources/js/COMPONENTS/accessibility-navbar.vue
+++ resources/js/COMPONENTS/accessibility-navbar.vue
@@ -8,16 +8,22 @@
 
                 <transition-group name="sub-menu-animation">
                     <a v-if="showSubmenu === false" href="#" class="content_link flex-grow text-left">>></a>
-                    <a :class="{ 'selected': selectedOption === 1 }" v-if="showSubmenu === false" @click="selectOption(1);" href="#" class="content_link flex-grow text-center">Option 1</a>
-                    <a :class="{ 'selected': selectedOption === 2 }" v-if="showSubmenu === false" @click="selectOption(2);" href="#" class="content_link flex-grow text-center">Option 2</a>
-                    <a :class="{ 'selected': selectedOption === 3 }" v-if="showSubmenu === false" @click="selectOption(3);" href="#" class="content_link flex-grow text-center">Option 3</a>
-                    <a :class="{ 'selected': selectedOption === 4 }" v-if="showSubmenu === false" @click="selectOption(4);" href="#" class="content_link flex-grow text-center">Option 4</a>
+                    <a v-if="showSubmenu === false" @click="selectOption(2);" href="#" class="content_link flex-grow text-center">
+                        <span :class="{ 'selected': selectedOption === 2 }">Option 2</span></a>
+                    <a v-if="showSubmenu === false" @click="selectOption(3);" href="#" class="content_link flex-grow text-center">
+                        <span :class="{ 'selected': selectedOption === 3 }">Option 3</span></a>
+                    <a v-if="showSubmenu === false" @click="selectOption(4);" href="#" class="content_link flex-grow text-center">
+                        <span :class="{ 'selected': selectedOption === 4 }">Option 4</span></a>
                 </transition-group>
                 <transition-group name="sub-menu-animation">
-                    <a :class="{ 'selected': selectedOption === 5 }" v-if="showSubmenu === true" @click="selectOption(5);" href="#" class="content_link flex-grow text-left">Option 5</a>
-                    <a :class="{ 'selected': selectedOption === 6 }" v-if="showSubmenu === true" @click="selectOption(6);" href="#" class="content_link flex-grow text-left">Option 6</a>
-                    <a :class="{ 'selected': selectedOption === 7 }" v-if="showSubmenu === true" @click="selectOption(7);" href="#" class="content_link flex-grow text-left">Option 7</a>
-                    <a :class="{ 'selected': selectedOption === 8 }" v-if="showSubmenu === true" @click="selectOption(8);" href="#" class="content_link flex-grow text-left">Option 8</a>
+                    <a v-if="showSubmenu === true" @click="selectOption(6);" href="#" class="content_link flex-grow text-left">
+                        <span :class="{ 'selected': selectedOption === 6 }">Option 6</span></a>
+                    <a v-if="showSubmenu === true" @click="selectOption(5);" href="#" class="content_link flex-grow text-left">
+                        <span :class="{ 'selected': selectedOption === 5 }">Option 5</span></a>
+                    <a v-if="showSubmenu === true" @click="selectOption(7);" href="#" class="content_link flex-grow text-left">
+                        <span :class="{ 'selected': selectedOption === 7 }">Option 7</span></a>
+                    <a v-if="showSubmenu === true" @click="selectOption(8);" href="#" class="content_link flex-grow text-left">
+                        <span :class="{ 'selected': selectedOption === 8 }">Option 8</span></a>
                     <a v-if="showSubmenu === true" href="#" class="content_link flex-grow text-left">>></a>
                     <a v-if="showSubmenu === true" href="#" class="content_link flex-grow text-center">...</a>
                 </transition-group>
@@ -53,10 +59,17 @@ export default {
 </script>
 
 <style scoped lang="sass">
+        @use "../../sass/abstracts/variables" as externall
+        @use '../../sass/abstracts' as *
+        @use "../../sass/base" as base
+
 
     $WidthBar: 70%
     $MaxHeightBar: 80px
-    $BackColBar: whitesmoke
+    $BackColBar: getColor(backgroundComponents, BasicWindow)
+    $FontColor: getColor(content, textTitle)
+    $FontOnHover: getColor(content, textHover)
+    $FontSelected: getColor(content, textActive)
     $BottomPaddingBar: 5px
     $BorderRadiusBar: 4px
     $PaddingDefaultBar: 25px
@@ -80,10 +93,13 @@ export default {
             // font-size: 20px
             font-style: normal
             font-weight: 700
-            line-height: 150%
+            line-height: 100%
+            color: $FontColor
+            span:hover
+                color: $FontOnHover
 
     .selected
-        color: blue
+        color: $FontSelected
 
     .sub-menu-content-off
         display: none
