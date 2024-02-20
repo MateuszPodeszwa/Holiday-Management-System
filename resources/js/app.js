058bd1cf8bf93a97bf4e3172604ba02bf9157402
Mateusz Podeszwa <podinatubie@gmail.com>
2024-02-20 23:01:13 +0000

    Add "resize" error handler

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,32 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-import * as VueRouter from "vue-router";
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+let Render = checkScreenSize(); // Set to false to render ErrorPage
+window.addEventListener('resize', checkScreenSize);
+// Function to check screen width and page height
+function checkScreenSize() {
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const pageHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    return screenWidth < 500 || pageHeight < 600;
+}
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => {
+        if (Render) {
+            return resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'))
+        } else {
+            return import('./Pages/ErrorPage.vue');
+        }
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
