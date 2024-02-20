09c51b2010f4c4b4d013610cc3241ce9a2d34329
Mateusz Podeszwa <podinatubie@gmail.com>
2024-02-20 23:00:20 +0000

    Create ErrorPage.vue

---
+++ resources/js/Pages/ErrorPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Error Page</h1>
+        <p>render off</p>
+        <!-- Add any additional content or styling for your error page -->
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+* {
+    background-color: black;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
