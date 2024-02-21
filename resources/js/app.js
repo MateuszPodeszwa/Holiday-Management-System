1bac529632e8a9bc96db6e533eac1b45e4acc162
Mateusz Podeszwa <podinatubie@gmail.com>
2024-02-21 13:35:10 +0000

    Update app.js
    Update the window size detector function, changed values and made to be more clear.

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,20 +8,20 @@ import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-let Render = checkScreenSize(); // Set to false to render ErrorPage
-window.addEventListener('resize', checkScreenSize);
 // Function to check screen width and page height
 function checkScreenSize() {
+    console.log('1');
     const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     const pageHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
 
-    return screenWidth < 500 || pageHeight < 600;
+    return screenWidth < 500 || pageHeight < 500;
 }
+window.addEventListener('resize', checkScreenSize);
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => {
-        if (Render) {
+        if (!checkScreenSize()) {
             return resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'))
         } else {
             return import('./Pages/ErrorPage.vue');
