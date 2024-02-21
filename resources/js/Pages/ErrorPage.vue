7464b500a187ea82980c0ae4e48a1169ffa3a31a
Mateusz Podeszwa <podinatubie@gmail.com>
2024-02-21 13:33:44 +0000

    Update ErrorPage.vue

--- resources/js/Pages/ErrorPage.vue
+++ resources/js/Pages/ErrorPage.vue
@@ -1,20 +1,36 @@
 <template>
-    <div>
-        <h1>Error Page</h1>
-        <p>render off</p>
-        <!-- Add any additional content or styling for your error page -->
+    <div class="error-page">
+        <h1>Your screen size is not supported</h1>
+        <p>Please resize your screen or use a device with a larger display.</p>
     </div>
 </template>
 
 <script>
 export default {
-}
+    name: 'ErrorPage',
+};
 </script>
 
 <style scoped>
-* {
-    background-color: black;
-    width: 100vw;
+.error-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     height: 100vh;
+    text-align: center;
+    font-family: 'Arial', sans-serif;
+    background-color: #f5f5f5;
+    color: #333;
+}
+
+h1 {
+    font-size: 2em;
+    margin-bottom: 10px;
+    color: #ff6347; /* Tomato color */
+}
+
+p {
+    font-size: 1.2em;
 }
 </style>
