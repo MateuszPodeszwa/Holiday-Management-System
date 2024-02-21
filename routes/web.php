6c8de6c78a0bbd27da39317eeb22f48b6e8d9f74
Mateusz Podeszwa <podinatubie@gmail.com>
2024-02-21 13:33:20 +0000

    Add comments / Remove logintest

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Inertia\Inertia;
 |
 */
 
+// I don't know what it does
 Route::get('/', function () {
-    return Inertia::render('Auth/Login', [
+    return Inertia::render('Auth/LoginPage', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -25,18 +26,24 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/t1', function () {
-   return Inertia::render('Auth/LoginTest');
-})->name('login-test');
+// Add /logintest page
+Route::get('/oldlogin', function () {
+   return Inertia::render('Auth/Login');
+})->name('login');
 
+// You can edit the login in the AuthenticatedSessionController.php
+
+// Only logged users can access dashboard
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Redirect all logged to deshboard
 Route::get('/', function () {
     return redirect()->route('dashboard'); // Redirect logged-in users to the dashboard
 })->middleware(['guest']);
 
+// Who knows what is this
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
