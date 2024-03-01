deed66dc5fdd30133a3e09592efbbfbd9eff4f27
Mateusz Podeszwa <podinatubie@gmail.com>
2024-03-01 15:11:05 +0000

    Create ProfilePicture.vue

---
+++ resources/js/Components/ProfilePicture.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="NAV-Profile_Picture">
+        <slot />
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    .NAV-Profile_Picture
+        border-radius: 50%
+        overflow: hidden
+        width: 40px
+        height: 40px
+        background: red
+</style>
+
