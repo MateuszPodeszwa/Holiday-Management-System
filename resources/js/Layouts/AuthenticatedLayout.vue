6d1bcd5b4cacd92d6ac2bcbcc44eaa9be1b05216
Mateusz Podeszwa <podinatubie@gmail.com>
2024-03-01 15:11:10 +0000

    Update AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -4,106 +4,41 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
+import ProfilePicture from '@/Components/ProfilePicture.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
+import FloatingBar from "@/Components/FloatingBar.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
     <div>
-        <div class="min-h-screen bg-gray-100">
-            <nav class="bg-white border-b border-gray-100">
+        <div class="min-h-screen bg-gray-500">
+            <!-- Header, The Strip-->
+            <nav class="NAV-Primary-Styles">
                 <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
-                                    />
-                                </Link>
-                            </div>
-
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
-                            </div>
-                        </div>
-
-                        <div class="hidden sm:flex sm:items-center sm:ms-6">
-                            <!-- Settings Dropdown -->
-                            <div class="ms-3 relative">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {{ $page.props.auth.user.name }}
-
-                                                <svg
-                                                    class="ms-2 -me-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
-
-                                    <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
-                                        <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <!-- Hamburger -->
-                        <div class="-me-2 flex items-center sm:hidden">
-                            <button
-                                @click="showingNavigationDropdown = !showingNavigationDropdown"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        :class="{
-                                            hidden: showingNavigationDropdown,
-                                            'inline-flex': !showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        :class="{
-                                            hidden: !showingNavigationDropdown,
-                                            'inline-flex': showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
+                <div class="NAV-Primary-Menu">
+                    <!-- Logo -->
+                    <div class="NAV-Left">
+                        <Link :href="route('dashboard')">
+                            <ApplicationLogo style="max-width: 140px" class="block h-9 fill-current text-gray-800"/>
+                        </Link>
                     </div>
+
+                    <!-- Navigation Links -->
+                    <div class="NAV-Middle">
+                        <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                            Dashboard
+                        </NavLink>
+                    </div>
+
+                    <div class="NAV-Right">
+                        <ProfilePicture>
+                            O
+                        </ProfilePicture>
+                    </div>
+
                 </div>
 
                 <!-- Responsive Navigation Menu -->
@@ -150,3 +85,51 @@ const showingNavigationDropdown = ref(false);
         </div>
     </div>
 </template>
+<style lang="sass" scoped>
+// Importing local files
+@use '../../sass/abstracts' as *
+@use "../../sass/base" as base
+@use "../../sass/abstracts/variables" as externall
+
+// Define local variables
+$MainBackgroundColor: getColor(background, primary)
+$SecondaryBackgroundColor: getColor(background, secondary)
+$MainBackgroundComponentColor: getColor(backgroundComponents, BasicWindow)
+$ColorTitle: getColor(content, textTitle)
+$LoginBoxBase: 520px
+$LoginBoxSize: (min(100vw, max($LoginBoxBase, 20vw)), min(fit-content, 100vh), $LoginBoxBase) // nth: 1: width, 2: height
+$BreakingPoint: externall.$MobileBreakingPoint
+$InputColor: getColor(content, inputBackground)
+
+// Controls the ribbon of the primary navbar
+.NAV-Primary-Styles
+    background: $SecondaryBackgroundColor
+    height: 80px
+    display: flex
+    justify-content: space-between
+
+// Controls the items inside the primary navbar
+.NAV-Primary-Menu
+    display: grid
+    grid-template-columns: 1fr 1fr 1fr
+    align-items: center
+    padding: 1rem
+
+.NAV-Left, .NAV-Middle, .NAV-Right
+    display: flex
+    align-items: center
+    justify-content: center
+    flex-direction: row
+    position: absolute
+
+.NAV-Right
+    right: 20px
+
+.NAV-Left
+    left: 20px
+
+.NAV-Middle
+    left: 50%
+    right: 50%
+
+</style>
