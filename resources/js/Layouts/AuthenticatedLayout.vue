e2f89e55d76d03627f5d69528af4b95f7278de95
Mateusz Podeszwa <podinatubie@gmail.com>
2024-03-01 16:06:54 +0000

    Update AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -13,7 +13,6 @@ const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-    <div>
         <div class="min-h-screen bg-gray-500">
             <!-- Header, The Strip-->
             <nav class="NAV-Primary-Styles">
@@ -22,53 +21,25 @@ const showingNavigationDropdown = ref(false);
                     <!-- Logo -->
                     <div class="NAV-Left">
                         <Link :href="route('dashboard')">
-                            <ApplicationLogo style="max-width: 140px" class="block h-9 fill-current text-gray-800"/>
+                            <ProfilePicture/>
                         </Link>
                     </div>
 
                     <!-- Navigation Links -->
                     <div class="NAV-Middle">
+                        <Link :href="route('dashboard')">
+                            <a>HOLIDAYS</a>
+                        </Link>
+                    </div>
+
+                    <div class="NAV-Right">
                         <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
                         </NavLink>
                     </div>
 
-                    <div class="NAV-Right">
-                        <ProfilePicture>
-                            O
-                        </ProfilePicture>
-                    </div>
-
                 </div>
 
-                <!-- Responsive Navigation Menu -->
-                <div
-                    :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
-                    class="sm:hidden"
-                >
-                    <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </ResponsiveNavLink>
-                    </div>
-
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200">
-                        <div class="px-4">
-                            <div class="font-medium text-base text-gray-800">
-                                {{ $page.props.auth.user.name }}
-                            </div>
-                            <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
-                        </div>
-
-                        <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>
-                            <ResponsiveNavLink :href="route('logout')" method="post" as="button">
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
             </nav>
 
             <!-- Page Heading -->
@@ -83,7 +54,6 @@ const showingNavigationDropdown = ref(false);
                 <slot />
             </main>
         </div>
-    </div>
 </template>
 <style lang="sass" scoped>
 // Importing local files
@@ -96,40 +66,32 @@ $MainBackgroundColor: getColor(background, primary)
 $SecondaryBackgroundColor: getColor(background, secondary)
 $MainBackgroundComponentColor: getColor(backgroundComponents, BasicWindow)
 $ColorTitle: getColor(content, textTitle)
-$LoginBoxBase: 520px
-$LoginBoxSize: (min(100vw, max($LoginBoxBase, 20vw)), min(fit-content, 100vh), $LoginBoxBase) // nth: 1: width, 2: height
 $BreakingPoint: externall.$MobileBreakingPoint
 $InputColor: getColor(content, inputBackground)
 
 // Controls the ribbon of the primary navbar
 .NAV-Primary-Styles
-    background: $SecondaryBackgroundColor
-    height: 80px
+    background: $SecondaryBackgroundColor   // The Colour of the NAV
+    height: 80px                            // Height of the NAV
+
+    // Makes the things to display in the perfect centre
     display: flex
-    justify-content: space-between
+    justify-content: center
+    align-items: center
 
 // Controls the items inside the primary navbar
 .NAV-Primary-Menu
-    display: grid
-    grid-template-columns: 1fr 1fr 1fr
-    align-items: center
-    padding: 1rem
+    display: flex
+    justify-content: space-between          // Divides the space between items equally
+    width: 95%                             // Ensure the menu takes up the full width
 
 .NAV-Left, .NAV-Middle, .NAV-Right
+    // Correct items to be on the vertical centre
     display: flex
     align-items: center
-    justify-content: center
-    flex-direction: row
-    position: absolute
-
-.NAV-Right
-    right: 20px
-
-.NAV-Left
-    left: 20px
 
 .NAV-Middle
-    left: 50%
-    right: 50%
+    flex-grow: 0
+
 
 </style>
