49ec60876996c433890e867fbd56ebc71a6d8015
Mateusz Podeszwa <podinatubie@gmail.com>
2024-03-05 20:22:28 +0000

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,18 +1,26 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import DropdownLink from "@/Components/DropdownLink.vue";
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-s
+        <template #header>
+            sup
+        </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
+                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
+                        <DropdownLink :href="route('logout')" method="post" as="button">
+                            Log Out
+                        </DropdownLink>
+                    <i class="fa-solid fa-square-q"></i>
                 </div>
             </div>
         </div>
