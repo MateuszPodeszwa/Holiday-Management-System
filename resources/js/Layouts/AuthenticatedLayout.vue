db2493fedcfa9747307fe5e13be8f6ee05b9c61e
Mateusz Podeszwa <podinatubie@gmail.com>
2024-03-07 00:37:46 +0000

    Update AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -9,6 +9,7 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
 import FloatingBar from "@/Components/FloatingBar.vue";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Breadcrumb from "@/Components/Breadcrumb.vue";
 const showingNavigationDropdown = ref(false);
 const appNameDashboard = import.meta.env.VITE_APP_NAME_FULL
 </script>
@@ -44,10 +45,9 @@ const appNameDashboard = import.meta.env.VITE_APP_NAME_FULL
                         <button class="bi bi-question-square-fill NAV-Icon"></button>
                         <button class="bi bi-archive-fill NAV-Icon"></button>
                         <button class="bi bi-bookmark-plus-fill NAV-Icon"></button>
-<!--                    <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </NavLink>
--->
+<!--                    <NavLink :href="route('dashboard')" :active="route().current('dashboard')">-->
+<!--                            Dashboard-->
+<!--                        </NavLink>-->
                     </div>
 
                 </div>
@@ -57,6 +57,9 @@ const appNameDashboard = import.meta.env.VITE_APP_NAME_FULL
             <!-- Page Heading -->
             <header class="shadow NAV-SUB" v-if="$slots.header">
                 <div>
+                    <span class="NAV-SUB-Left_side">
+                        <breadcrumb />
+                    </span>
                     <slot name="header" />
                 </div>
             </header>
@@ -105,10 +108,12 @@ $InputColor: getColor(content, inputBackground)
     flex-grow: 0
     transform: translateX(-50%)
     // margin-left: perfect-horizontal-center(0)
+
     @media (width < 900px)
         display: none
     @media (width < 1150px)
         transform: translateX(-20%)
+
     a
         color: lighten($ColorTitle, 100%)
         font-family: Verdana, Geneva, sans-serif
@@ -192,7 +197,7 @@ $InputColor: getColor(content, inputBackground)
         display: grid
         place-items: center start
         grid-template-columns: 1fr 1fr
-        padding: 0 externall.$DashboardNavbarSideMargins + externall.$SearchBarSpacing + 15px
+        padding: 0 35px 0 externall.$DashboardNavbarSideMargins + externall.$SearchBarSpacing - 7px // Was + 15
         @media (width < 580px)
             padding: 0 externall.$DashboardNavbarSideMargins - 30px
 </style>
