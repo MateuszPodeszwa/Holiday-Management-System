810bed633c8e18fa3988b1e355ca060b6e4b8a82
Mateusz Podeszwa <podinatubie@gmail.com>
2024-03-07 00:37:57 +0000

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,6 +3,7 @@ import Breadcrumb from "@/Components/Breadcrumb.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
 import { Head } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Link} from "@inertiajs/vue3";
 
 export default {
     name: 'Dashboard',
@@ -11,6 +12,7 @@ export default {
         DropdownLink,
         AuthenticatedLayout,
         Head,
+        Link,
     }
 }
 </script>
@@ -20,8 +22,10 @@ export default {
 
     <AuthenticatedLayout>
         <template #header>
-            <div class="NAV-SUB-Left_side"><breadcrumb/></div>
-            <div class="NAV-SUB-Right_side">sup2</div>
+            <div class="NAV-SUB-Right_side">
+                <Link :href="route('profile.edit')"><button class="SUB-NAV-Button">Settings</button></Link>
+                <Link :href="route('logout')" method="post"><button class="SUB-NAV-Button">Logout</button></Link>
+            </div>
         </template>
 
         <div class="py-12">
@@ -38,3 +42,41 @@ export default {
         </div>
     </AuthenticatedLayout>
 </template>
+
+<style lang="sass" scoped>
+// Importing local files
+@use '../../sass/abstracts' as *
+// @use "../../sass/base" as base
+@use "../../sass/abstracts/variables" as externall
+
+// Define local variables
+$MainBackgroundColor: getColor(background, primary)
+$SecondaryBackgroundColor: getColor(background, secondary)
+$MainBackgroundComponentColor: getColor(backgroundComponents, BasicWindow)
+$ColorTitle: getColor(content, textTitle)
+$InputColor: getColor(content, inputBackground)
+
+.NAV-SUB-Right_side
+    justify-self: end /* Align to the right horizontally */
+    align-self: end /* Align to the top vertically */
+    width: fit-content
+    height: 100%
+    display: grid
+    grid-template-columns: 1fr 1fr
+    place-items: center end
+    gap: 25px
+
+    button
+        width: 100px
+        height: 35px
+        color: $ColorTitle
+        background: $InputColor
+        border-radius: externall.$BorderRadius / 2
+        transition: 250ms all ease
+
+        &:hover
+            color: $InputColor
+            background: $SecondaryBackgroundColor
+            transition: 250ms all ease
+
+</style>
