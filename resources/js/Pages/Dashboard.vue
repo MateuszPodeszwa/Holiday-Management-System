be890b7f44da08d46b1dfb9d85f43524e76d89ec
Mateusz Podeszwa <podinatubie@gmail.com>
2024-03-13 09:15:28 +0000

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,6 +8,7 @@ import {Link} from "@inertiajs/vue3";
 export default {
     name: 'Dashboard',
     components: {
+
         Breadcrumb,
         DropdownLink,
         AuthenticatedLayout,
@@ -28,17 +29,13 @@ export default {
             </div>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="py-12 bg-white">
                     <div class="p-6 text-gray-900">You're logged in!</div>
                         <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
                         <DropdownLink :href="route('logout')" method="post" as="button">
                             Log Out
                         </DropdownLink>
                     <i class="fa-solid fa-square-q"></i>
-                </div>
-            </div>
         </div>
     </AuthenticatedLayout>
 </template>
@@ -46,8 +43,9 @@ export default {
 <style lang="sass" scoped>
 // Importing local files
 @use '../../sass/abstracts' as *
-// @use "../../sass/base" as base
+@use "../../sass/base" as base
 @use "../../sass/abstracts/variables" as externall
+@use "../../sass/components/SubmenuButtonComponent" as subMenu
 
 // Define local variables
 $MainBackgroundColor: getColor(background, primary)
@@ -55,28 +53,4 @@ $SecondaryBackgroundColor: getColor(background, secondary)
 $MainBackgroundComponentColor: getColor(backgroundComponents, BasicWindow)
 $ColorTitle: getColor(content, textTitle)
 $InputColor: getColor(content, inputBackground)
-
-.NAV-SUB-Right_side
-    justify-self: end /* Align to the right horizontally */
-    align-self: end /* Align to the top vertically */
-    width: fit-content
-    height: 100%
-    display: grid
-    grid-template-columns: 1fr 1fr
-    place-items: center end
-    gap: 25px
-
-    button
-        width: 100px
-        height: 35px
-        color: $ColorTitle
-        background: $InputColor
-        border-radius: externall.$BorderRadius / 2
-        transition: 250ms all ease
-
-        &:hover
-            color: $InputColor
-            background: $SecondaryBackgroundColor
-            transition: 250ms all ease
-
 </style>
